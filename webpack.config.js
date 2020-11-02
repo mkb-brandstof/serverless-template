c43b467966245423452713f0e9bf79ff2fabb991
CodeVision <codevision@gmail.com>
2020-11-02 15:21:13 +0100

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const ServerlessCopyWebpackPlugin = require('serverless-copy-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const slsw = require('serverless-webpack');
+
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node12',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+    minimize: ['dev', 'test'].includes(slsw.lib.serverless.variables.options.environment)
+  },
+  plugins: [
+    new ServerlessCopyWebpackPlugin()
+  ],
+  externals: [nodeExternals()]
+}
