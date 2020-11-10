3eafda8f493b46ba20741bcfa7d29336a19211a5
CodeVision <codevision@gmail.com>
2020-11-10 19:48:15 +0100

    Confirm to our own linting rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const ServerlessCopyWebpackPlugin = require('serverless-copy-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 const slsw = require('serverless-webpack');
@@ -8,10 +10,10 @@ module.exports = {
   target: 'node12',
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   optimization: {
-    minimize: ['dev', 'test'].includes(slsw.lib.serverless.variables.options.environment)
+    minimize: ['dev', 'test'].includes(slsw.lib.serverless.variables.options.environment),
   },
   plugins: [
-    new ServerlessCopyWebpackPlugin()
+    new ServerlessCopyWebpackPlugin(),
   ],
-  externals: [nodeExternals()]
-}
+  externals: [nodeExternals()],
+};
